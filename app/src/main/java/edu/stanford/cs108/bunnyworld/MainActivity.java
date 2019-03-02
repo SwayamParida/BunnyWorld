6be6085eadd45938abc763ee8208b0e74bd9353f
kivaluramanlal <kivalu@stanford.edu>
2019-03-02 15:56:13 -0800

    Added new folders to res. Put images in drawable/Media for user-approved images Created home screen

--- app/src/main/java/edu/stanford/cs108/bunnyworld/MainActivity.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/MainActivity.java
@@ -1,7 +1,19 @@
 package edu.stanford.cs108.bunnyworld;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import static android.view.View.SYSTEM_UI_FLAG_FULLSCREEN;
+import static android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+import static android.view.View.SYSTEM_UI_FLAG_IMMERSIVE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +22,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        VideoView myVideoView = (VideoView) findViewById(R.id.launchVideoView);
+        myVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.bunny_video));
+        myVideoView.setMediaController(new MediaController(this));
+        myVideoView.start();
+
+        myVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.start();
+
+            }
+        });
+        getWindow().getDecorView().setSystemUiVisibility(SYSTEM_UI_FLAG_IMMERSIVE|
+                SYSTEM_UI_FLAG_FULLSCREEN|SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    public void openEditChooser(View view) {
+        Intent intent = new Intent(this, OpenEditActivity.class);
+        startActivity(intent);
     }
 
     //This is Anmol
