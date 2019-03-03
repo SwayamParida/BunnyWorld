c9b77d0dc3e60fb0ab938e58fa0a06816c521444
kivaluramanlal <kivalu@stanford.edu>
2019-03-02 18:36:35 -0800

    Finished up start screen and edit game selector.

--- app/src/main/java/edu/stanford/cs108/bunnyworld/OpenEditActivity.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/OpenEditActivity.java
@@ -1,7 +1,13 @@
 package edu.stanford.cs108.bunnyworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+
+import static android.view.View.SYSTEM_UI_FLAG_FULLSCREEN;
+import static android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+import static android.view.View.SYSTEM_UI_FLAG_IMMERSIVE;
 
 public class OpenEditActivity extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class OpenEditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_edit);
+        getWindow().getDecorView().setSystemUiVisibility(SYSTEM_UI_FLAG_IMMERSIVE|
+                SYSTEM_UI_FLAG_FULLSCREEN|SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
+
+    @Override
+    public void onBackPressed() {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+    }
+
 }
