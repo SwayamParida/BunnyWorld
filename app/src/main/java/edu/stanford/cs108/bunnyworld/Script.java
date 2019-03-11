9a8028da10321579b53ffc8ee4a625d7686afd7b
Swayam Parida <swayamp@stanford.edu>
2019-03-10 18:10:14 -0700

    Updated PageEditorActivity UI. Fixed Script's toString(). - Fixed the formatting of the Script's string. - Moved buttons down to near the scroll view - Added Spinners to attribute inspector to add Script attributes.

--- app/src/main/java/edu/stanford/cs108/bunnyworld/Script.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/Script.java
@@ -8,6 +8,11 @@ public class Script {
     private boolean onClick, onEnter, onDrop;
     private List<Action> onClickActions, onEnterActions, onDropActions;
 
+    private final static String VERB_MODIFIER_DELIMITER = " ";
+    private final static String ACTION_DELIMITER = "; ";
+    private final static String TRIGGER_DELIMITER = "}\n ";
+    private final static String regex = "trigger: { (verb modifier; )*}\n";
+
     public Script(boolean onClick, boolean onEnter, boolean onDrop,
                   List<Action> onClickActions, List<Action> onDropActions, List<Action> onEnterActions) {
         this.onClick = onClick;
@@ -18,21 +23,33 @@ public class Script {
         this.onEnterActions = onEnterActions;
     }
 
+    public Script parseScript(String scriptString) {
+        return null; //TODO: Figure out how to use RegEx to parse String
+    }
+
     @Override
     public String toString() {
         StringBuilder scriptBuilder = new StringBuilder();
-        addIf(onClick, onClickActions, scriptBuilder);
-        addIf(onDrop, onDropActions, scriptBuilder);
-        addIf(onEnter, onEnterActions, scriptBuilder);
+        add(onClick, "onClick: { ", onClickActions, scriptBuilder);
+        add(onDrop, "onDrop: { ", onDropActions, scriptBuilder);
+        add(onEnter, "onEnter: { ", onEnterActions, scriptBuilder);
         return scriptBuilder.toString();
     }
 
-    private void addIf (boolean shouldAdd, Object toAdd, StringBuilder stringBuilder) {
-        if (shouldAdd)
-            stringBuilder.append(toAdd);
+    private void add(boolean shouldAdd, String actionLabel, Object toAdd, StringBuilder stringBuilder) {
+        if (!shouldAdd) return;
+
+        stringBuilder.append(actionLabel);
+        stringBuilder.append(VERB_MODIFIER_DELIMITER);
+        stringBuilder.append(toAdd.toString());
+        stringBuilder.append(ACTION_DELIMITER);
     }
-    private void addIf(boolean shouldAdd, List toAdd, StringBuilder stringBuilder) {
-        toAdd.forEach(elem -> addIf(shouldAdd, elem, stringBuilder));
+    private void add(boolean shouldAdd, String triggerLabel, List<Action> toAdd, StringBuilder stringBuilder) {
+        if (!shouldAdd) return;
+
+        stringBuilder.append(triggerLabel);
+        toAdd.forEach(action -> stringBuilder.append(action));
+        stringBuilder.append(TRIGGER_DELIMITER);
     }
 
     public class Action {
@@ -57,9 +74,10 @@ public class Script {
         @Override
         public String toString() {
             StringBuilder scriptBuilder = new StringBuilder();
-            addIf(goTo, gotoPage, scriptBuilder);
-            addIf(play, audio, scriptBuilder);
-            addIf(hide, hideShape, scriptBuilder);
+            add(goTo, "goto ", gotoPage, scriptBuilder);
+            add(play, "play ", audio, scriptBuilder);
+            add(hide, "hide ", hideShape, scriptBuilder);
+            add(show, "show ", showShape, scriptBuilder);
             return scriptBuilder.toString();
         }
     }
